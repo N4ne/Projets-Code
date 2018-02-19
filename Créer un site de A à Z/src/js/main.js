37e02beeb75c82e1ea67b8794282c8c494571971
N4ne <31614379+N4ne@users.noreply.github.com>
2018-02-19 16:22:27 +0100

    Mon Portfolio 2018 MA

---
+++ Créer un site de A à Z/src/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  $('#open_modal').click(function() {
+    $('#modal_to_open').css(
+      {
+        'display': 'block'
+      }
+    );
+  });
+
+  $('#close_modal').click(function() {
+    $('#modal_to_open').css(
+      {
+        'display': 'none'
+      }
+    );
+  });
+
+//   $('#send_email').click(function(e){
+		// e.preventDefault();
+		// var data = {
+			// email: $('#email').val(),
+			// name: $('#name').val(),
+			// objet: $('#firstname').val(),
+			// message: $('#message').val()
+		// };
+		// $.ajax({
+			// url: "mail.php",
+			// type: 'POST',
+			// data: data,
+			// success: function(data) {
+				// $('#js_alert_success').css({'display' : 'block'});
+				// setTimeout(function(){
+				// 	$('#js_alert_success').css({'display' : 'none'});
+					// $('#email').val("");
+					// $('#name').val("");
+					// $('#firstname').val("");
+					// $('#message').val("")
+				// }, 3000);
+		// 	},
+		// 	error: function(data) {
+				// $('#js_alert_danger').css({'display' : 'block'});
+				// setTimeout(function(){
+				// 	$('#js_alert_danger').css({'display' : 'none'});
+				// 	$('#email').val("");
+					// $('#name').val("");
+					// $('#firstname').val("");
+					// $('#message').val("")
+				// }, 3000);
+			// }
+		// });
+	// });
+
+});
